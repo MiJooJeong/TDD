e625f0ccbd760b736f9af9df2d93302247132631
MiJooJeong <uuuio1224@gmail.com>
2018-06-07 16:25:44 +0900

    새로운 List 객체를 생성

--- lists/tests.py
+++ lists/tests.py
@@ -3,8 +3,8 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 from django.test import TestCase
 
+from lists.models import Item, List
 from lists.views import home_page
-from lists.models import Item
 
 
 # Create your tests here.
@@ -22,24 +22,33 @@ class HomePageTest(TestCase):
         self.assertEqual(response.content.decode(), expected_html)
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = '첫 번째 아이템'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = '두 번째 아이템'
+        second_item.list = list_
         second_item.save()
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
 
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, first_item.text)
-        self.assertEqual(second_saved_item.text, second_item.text)
+        self.assertEqual(first_saved_item.text, '첫 번째 아이템')
+        self.assertEqual(first_saved_item.list, list_)
+        self.assertEqual(second_saved_item.text, '두 번째 아이템')
+        self.assertEqual(second_saved_item.list, list_)
 
 
 class ListViewTest(TestCase):
@@ -48,6 +57,16 @@ class ListViewTest(TestCase):
         response = self.client.get('/lists/the-only-list-in-the-world/')
 
         self.assertTemplateUsed(response, 'list.html')
+
+    def test_displays_all_items(self):
+        list_ = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list_)
+        Item.objects.create(text='itemey 2', list=list_)
+
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
 
 
 class NewListTest(TestCase):
--- lists/views.py
+++ lists/views.py
@@ -1,20 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
-from lists.models import Item
+from lists.models import Item, List
 
 
 # Create your views here.
-# def home_page(request):
-#     if request.method == 'POST':
-#         new_item_text = request.POST['item_text']
-#         Item.objects.create(text=new_item_text)
-#
-#     else:
-#         new_item_text = ''
-#
-#     return render(request, 'home.html', {
-#         'new_item_text': new_item_text,
-#     })
 
 def home_page(request):
     return render(request, 'home.html')
@@ -26,5 +15,6 @@ def view_list(request):
 
 
 def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/the-only-list-in-the-world/')
