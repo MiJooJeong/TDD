9f453b5f2dc8913b7e0c3c357615c0f0b58cbc69
MiJooJeong <uuuio1224@gmail.com>
2018-07-04 19:22:32 +0900

    모델 검증을 위한 new list 뷰 수정

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,7 @@
+from django.core.exceptions import ValidationError
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
-from lists.models import Item, List
+from .models import Item, List
 
 
 # Create your views here.
@@ -16,8 +17,16 @@ def view_list(request, list_id):
 
 def new_list(request):
     list_ = List.objects.create()
-    Item.objects.create(text=request.POST['item_text'], list=list_)
+    item = Item(text=request.POST['item_text'], list=list_)
+    try:
+        item.full_clean()
+        item.save()
+    except ValidationError:
+        list_.delete()
+        error = "빈 아이템을 등록할 수 없습니다."
+        return render(request, 'home.html', {"error": error})
     return redirect('/lists/%d/' % (list_.id,))
+
 
 def add_item(request, list_id):
     list_ = List.objects.get(id=list_id)
