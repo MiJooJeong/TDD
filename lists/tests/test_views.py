e632685f7cee6740c7982a05807e8d25d5cb6a38
MiJooJeong <uuuio1224@gmail.com>
2018-07-05 16:48:44 +0900

    고유성 검증 처리를 위한 form 작성

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,11 +1,8 @@
-from django.core.urlresolvers import resolve
-from django.http import HttpRequest
-from django.template.loader import render_to_string
 from django.test import TestCase
 from django.utils.html import escape
+from unittest import skip
 
 from ..models import Item, List
-from ..views import home_page
 from ..forms import ItemForm, EMPTY_LIST_ERROR
 
 
@@ -115,6 +112,19 @@ class ListViewTest(TestCase):
     def test_for_invalid_input_shows_error_on_page(self):
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_LIST_ERROR))
+
+    @skip
+    def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text='textey')
+        response = self.client.post(
+            '/lists/%d/' % (list1.id,),
+            data={'text': 'textey'}
+        )
+        expected_error = escape("이미 리스트에 해당 아이템이 있습니다.")
+        self.assertContains(response, expected_error)
+        self.assertTemplateUsed(response, 'list.html')
+        self.assertEqual(Item.objects.all().count(), 1)
 
 
 class NewListTest(TestCase):
